b9a7b2edf8ca6633a39f318586de5f277265c3d0
luezu-42 <leonardouezu@gmail.com>
2021-04-12 00:02:09 -0300

    Fix: header

--- src/server/app.js
+++ src/server/app.js
@@ -12,20 +12,7 @@ app.use(helmet());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type'
-  );
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
-app.use(cors({ origin: '*' }));
+app.use(cors());
 require('../database/index')();
 
 app.use('/comic', comic);
