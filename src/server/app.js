599925b8f5724ac749f09c9d6827d6796ec3dc38
luezu-42 <leonardouezu@gmail.com>
2021-04-11 23:55:11 -0300

    Fix: header

--- src/server/app.js
+++ src/server/app.js
@@ -12,6 +12,19 @@ app.use(helmet());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.use(cors({ origin: '*' }));
 require('../database/index')();
 
